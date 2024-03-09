a4cb188b5bf8463ee6d3cbcec84445a48923720f
Elliott Larsen <elliottlrsn@gmail.com>
2024-03-08 19:27:37 -0700

    Receive method first pass.

--- src/main/java/org/zerock/chatterup/Client.java
+++ src/main/java/org/zerock/chatterup/Client.java
@@ -1,5 +1,7 @@
 package org.zerock.chatterup;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -10,13 +12,48 @@ public class Client {
         this.socket = socket;
         receive();
     }
-    // Method to receive messages from a client.
     public void receive() {
+        // Method to receive messages from a client.
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        InputStream in = socket.getInputStream();
+                        byte[] buffer = new byte[512];
+
+                        int length = in.read(buffer);
+                        if(length == -1) {
+                            throw new IOException();
+                        }
+                        System.out.println("[Message received] "
+                                + socket.getRemoteSocketAddress() + ": "
+                                + Thread.currentThread().getName());
+
+                        String message = new String(buffer, 0, length, "UTF-8");
+                        for (Client client : Main.clients) {
+                            client.send(message);
+                        }
+                    }
+                } catch (Exception e) {
+                    try {
+                        System.out.println("[Message receive error] "
+                                + socket.getRemoteSocketAddress() + ": "
+                                + Thread.currentThread().getName());
+                        Main.clients.remove(Client.this);
+                        socket.close();
+                    } catch (Exception e2) {
+                        e2.printStackTrace();
+                    }
+                }
+            }
+        };
+        Main.threadPool.submit(thread);
 
     }
 
-    // Method to send messages to a client.
     public void send(String message) {
+        // Method to send messages to a client.
         Runnable thread = new Runnable() {
             @Override
             public void run() {
