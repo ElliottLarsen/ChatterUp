f7d350e366062e2db62132fbd761f60a4f9381b3
Elliott Larsen <elliottlrsn@gmail.com>
2024-03-09 18:37:21 -0700

    UI first pass.

--- src/main/java/org/zerock/chatterup/Main.java
+++ src/main/java/org/zerock/chatterup/Main.java
@@ -10,6 +10,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 public class Main extends Application {
     public static ExecutorService threadPool;
@@ -79,7 +86,46 @@ public class Main extends Application {
     // Method to create UI and starts the application.
     @Override
     public void start(Stage primaryStage) {
+        // Set up for the UI.
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(5));
 
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Ariel", 15));
+        root.setCenter(textArea);
+
+        Button toggleButton = new Button("Start");
+        toggleButton.setMaxWidth((Double.MAX_VALUE));
+        BorderPane.setMargin(toggleButton, new Insets(1, 0, 0, 0));
+        root.setBottom(toggleButton);
+
+        String IP = "127.0.0.1";
+        int port = 8005;
+
+        // Start/Stop the server and change the button accordingly.
+        toggleButton.setOnAction(event -> {
+            if (toggleButton.getText().equals("Start")) {
+                startServer(IP, port);
+                Platform.runLater(() -> {
+                    String message = String.format("[Start Server]\n", IP, port);
+                    textArea.appendText(message);
+                    toggleButton.setText("Stop");
+                });
+            } else {
+                stopServer();
+                Platform.runLater(() -> {
+                    String message = String.format("[Stop Server]\n", IP, port);
+                    textArea.appendText(message);
+                    toggleButton.setText("Start");
+                });
+            }
+        });
+        Scene scene = new Scene(root, 400, 400);
+        primaryStage.setTitle("[ChatterUp Server]");
+        primaryStage.setOnCloseRequest(event -> stopServer());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     // Main method,
