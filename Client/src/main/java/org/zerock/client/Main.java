209f0eaeb859fde8f5a5ba5ccda13a5279bbf19c
Elliott Larsen <elliottlrsn@gmail.com>
2024-03-10 19:30:37 -0600

    send(), receive(), and stopClient().

--- Client/src/main/java/org/zerock/client/Main.java
+++ Client/src/main/java/org/zerock/client/Main.java
@@ -1,5 +1,9 @@
 package org.zerock.client;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import javafx.application.Application;
@@ -31,15 +35,49 @@ public class Main extends Application {
     }
     // Method to stop client.
     public void stopClient() {
-
+        try {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     // Method to receive messages from the server.
     public void receive() {
-
+        while (true) {
+            try {
+                InputStream in = socket.getInputStream();
+                byte[] buffer = new byte[512];
+                int length = in.read(buffer);
+                if (length == -1) {
+                    throw new IOException();
+                }
+                String message = new String(buffer, 0, length, "UTF-8");
+                Platform.runLater(() -> {
+                    textArea.appendText(message);
+                });
+            } catch (Exception e) {
+                stopClient();
+                break;
+            }
+        }
     }
     // Method to send messages to the server.
     public void send(String message) {
-
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    OutputStream out = socket.getOutputStream();
+                    byte[] buffer = message.getBytes("UTF-8");
+                    out.write(buffer);
+                    out.flush();
+                } catch (Exception e) {
+                    stopClient();
+                }
+            }
+        };
+        thread.start();
     }
     // Method to start the application.
     @Override
