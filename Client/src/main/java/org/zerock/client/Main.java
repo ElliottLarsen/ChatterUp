06f16e4089a0a26b8f2ca52c955ad3bc77c5e500
Elliott Larsen <elliottlrsn@gmail.com>
2024-03-10 19:25:51 -0600

    startClient() first pass.

--- Client/src/main/java/org/zerock/client/Main.java
+++ Client/src/main/java/org/zerock/client/Main.java
@@ -5,32 +5,48 @@ import java.net.Socket;
 import javafx.application.Application;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
-public class Main extends Application {
+import javafx.application.Platform;
 
+public class Main extends Application {
     Socket socket;
     TextArea textArea;
 
+    // Method to start client.
     public void startClient(String IP, int port) {
-
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    socket = new Socket(IP, port);
+                    receive();
+                } catch (Exception e) {
+                    if(!socket.isClosed()) {
+                        stopClient();
+                        System.out.println("[Cannot connect to the server]");
+                        Platform.exit();
+                    }
+                }
+            }
+        };
+        thread.start();
     }
-
+    // Method to stop client.
     public void stopClient() {
 
     }
-
+    // Method to receive messages from the server.
     public void receive() {
 
     }
-
+    // Method to send messages to the server.
     public void send(String message) {
 
     }
-
+    // Method to start the application.
     @Override
     public void start(Stage primaryMessage) {
 
     }
-
+    // Main method.
     public static void main(String[] args) {
         launch(args);
     }
